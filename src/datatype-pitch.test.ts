a3f052a7177777f7c8654990377c1d1d34fc8170
Christopher Nascone <chris@nasc.one>
2021-08-30 08:40:09 -0400

    fromString factory method

--- src/datatype-pitch.test.ts
+++ src/datatype-pitch.test.ts
@@ -6,12 +6,24 @@ describe('Pitch class', () => {
   describe('getters and setters', () => {
     beforeEach(() => {
       p = new Pitch(0, 4);
-    })
+    });
     it('returns the pitchClass it is initialized with', () => {
       expect(p.pitchClass).toBe(0);
-    })
+    });
     it('returns the register it is initialized with', () => {
       expect(p.register).toBe(4);
-    })
-  })
-})
+    });
+  });
+
+  describe('fromString factory method', () => {
+    it.each([
+      ['C4', 0, 4],
+      ['Eb3', 3, 3],
+      ['Cb4', 11, 3],
+    ])('fromString("%s") has a pitchClass of %d and a register of %d.', (input, expectedPitchClass, expectedRegister) => {
+      const p = Pitch.fromString(input);
+      expect(p.pitchClass).toBe(expectedPitchClass);
+      expect(p.register).toBe(expectedRegister);
+    });
+  });
+});
--- src/datatype-pitch.ts
+++ src/datatype-pitch.ts
@@ -4,9 +4,65 @@
  */
 const NUM_PITCH_CLASSES = 12;
 
+/**
+ * Matches, for e.g., C#4 or Eb3.
+ */
+const PITCH_REGEXP = /^([ABCDEFG])(#|B)?(\d+)$/;
+
+/**
+ * Man, what I would give to make this just 12 letters in a row with
+ * no sharps and flats.
+ */
+const NATURAL_INDICES = {
+  C: 0,
+  D: 2,
+  E: 4,
+  F: 5,
+  G: 7,
+  A: 9,
+  B: 11,
+};
+
+const MODIFIER_VALUES = {
+  B: -1,
+  '#': 1,
+};
+
+/**
+ * JS does modular arithmetic weirdly in the presence of negative numbers.
+ */
+function mod(n: number, m: number) {
+  return ((n % m) + m) % m;
+}
+
 export class Pitch {
   private _pitchClass: number;
   private _register: number;
+
+  static fromString(pitchSpec: string): Pitch {
+    const groups = pitchSpec.toUpperCase().match(PITCH_REGEXP);
+
+    if (!groups) {
+      throw new Error(`Cannot build Pitch from "${pitchSpec}".`);
+    }
+
+    const [_input, note, modifier, register] = groups;
+    const baseNoteValue = NATURAL_INDICES[note as keyof typeof NATURAL_INDICES];
+    const modifierValue =
+      MODIFIER_VALUES[modifier as keyof typeof MODIFIER_VALUES] || 0;
+    const sum = baseNoteValue + modifierValue;
+    let registerValue = parseInt(register);
+
+    if (sum < 0) {
+      --registerValue;
+    } else if (sum > NUM_PITCH_CLASSES) {
+      ++registerValue;
+    }
+
+    const pitchClass = mod(sum, NUM_PITCH_CLASSES);
+
+    return new this(pitchClass, registerValue);
+  }
 
   constructor(pitchClass: number, register: number) {
     this._pitchClass = pitchClass;
